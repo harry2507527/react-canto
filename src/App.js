a5852707c9869bb1aeb2ffaf857bf955d8ba3fcb
陳翰揚 <chenhanyang@chenhanyangs-MacBook-Pro.local>
2019-09-05 17:54:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { useSpeechSynthesis } from "react-speech-kit";
+import { Button, Container, Jumbotron, Navbar, Row, Col } from 'react-bootstrap';
+
 
 function App() {
+  const [text, setText] = useState('我是粵語學習機');
+  const [voiceIndex, setVoiceIndex] = useState(null);
+  
+  const onEnd = () => {
+    // You could do something here after speaking has finished
+  };
+  const {
+    speak,
+    cancel,
+    speaking,
+    supported,
+    voices
+  } = useSpeechSynthesis({ onEnd });
+
+  const voice = voices[voiceIndex] || voices[35];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">粵語學習機</Navbar.Brand>
+      </Navbar>
+      <Jumbotron>
+      <Container>
+
+      <form>
+        <h2>普通話轉粵語</h2>
+        { !supported && (
+          <p>Oh no, it looks like your browser doesn&#39;t support Speech Synthesis.</p>
+          )}
+        {supported && (
+          <React.Fragment>
+            
+
+            <label htmlFor="voice">
+              選擇zh-HK 
+            </label>
+            
+            <select
+              id="voice"
+              name="voice"
+              value={voiceIndex || ''}
+              onChange={(event) => { setVoiceIndex(event.target.value); }}
+              native 
+              >
+               
+              <option value="">zh-HK</option>
+              {voices.map((option, index) => (
+                <option key={option.voiceURI} value={index}>
+                  {`${option.lang} - ${option.name}`}
+                </option>
+              ))}
+            </select>
+            <p></p>
+            <label htmlFor="message">
+              輸入文字
+            </label>
+            <p></p>
+            <Row>
+            <Col>
+
+            <textarea
+              id="message"
+              name="message"
+              rows={10}
+              value={text}
+              onChange={(event) => { setText(event.target.value); }}
+              />
+              </Col>
+              <Col>
+              <p>{`${text}`}</p>
+              
+              </Col>
+              </Row>
+            { speaking
+              ? (
+                <Button type="button" onClick={cancel}>
+                  Stop
+                </Button>
+              ) : (
+                <Button type="button" onClick={() => speak({ text, voice })}>
+                  Speak
+                </Button>
+              )
+            }
+          </React.Fragment>
+        )}
+      </form>
+      
+        </Container>
+      </Jumbotron>
+    </>
   );
 }
 
